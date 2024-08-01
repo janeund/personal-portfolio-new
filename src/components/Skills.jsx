2043d3ed05305a606fec74f68f520ed6ba9c7212
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-08-01 15:20:16 +0200

    feat: add styles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,18 @@
 import { techSkills } from "../data";
 
+
 function Skills() {
   return (
-    <div>
-      <h2>Technologies and Tools</h2>
-      <ul>
+    <section className="mb-10 max-w-4xl container">
+      <h2 className="font-medium text-3xl mb-5">Technologies and Tools</h2>
+      <ul className="flex">
         {techSkills.map((skill, index) => (
-          <li key={skill.id} className={`cursor-pointer text-sm ${index === techSkills.length - 1 ? 'mr-0' : 'mr-10'}`}>
+          <li key={skill.id} className={`cursor-pointer p-3 bg-stone-200 text-base rounded-lg ${index === techSkills.length - 1 ? 'mr-0' : 'mr-8'}`}>
             {skill.name}
           </li>
         ))}
       </ul>
-    </div>
+    </section>
   )
 }
 
