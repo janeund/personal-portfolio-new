86404956d9e491f252a35079d1fbfe3b091cc0e3
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-08-09 22:26:02 +0200

    feat: add gradient background image on the top of page

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,14 @@ import photo from "./photo.jpg";
 import CV from "./CV_Evgeniia_Zhavoronkova.pdf";
 import landing from "./landing.png";
 import news from "./news.png";
+import topSvg from './top.svg';
+import top1Svg from './top1.svg';
 
 
 export {
   photo,
   CV,
-  news
+  news,
+  topSvg,
+  top1Svg
 }
