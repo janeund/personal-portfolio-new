c7bccaad3eeb97fd09a13f747191524082114b46
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-08-19 11:02:21 +0200

    feat: update navbar and hero styles, change info in experience section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { photo, CV } from "../assets";
+import { RxDownload } from "react-icons/rx";
 import Skills from "./Skills";
 import Social from "./Social";
 
@@ -6,16 +7,17 @@ function Hero() {
   return (
     <section id="hero" className='container mx-auto max-w-xl mb-20 z-10 relative'>
       <div className="flex justify-between">
-        <div className="info max-w-sm">
-          <h1 className="text-3xl font-medium">Hello! I'm Evgeniia, </h1>
-          <p className="font-medium ">Junior Frontend Developer based in Oslo.</p>
-          <p className="mt-2">I'm a self-taught developer with backround in video games marketing.</p>
+        <div className="info max-w-sm mb-4">
+          <h1 className="text-4xl font-medium">Hello! I'm Evgeniia, </h1>
+          <p className="font-medium text-lg">Junior Frontend Developer based in Oslo.</p>
+          <p className="mt-2 text-base">I'm a self-taught developer with backround in video games marketing.</p>
         </div>
-        <img src={photo} alt="profile photo" className="max-w-32 rounded-full border-2 border-black"/>
+        <img src={photo} alt="profile photo" className="shadow-xl max-w-32 max-h-32 rounded-full border border-gray-400"/>
       </div>
-      <div>
+      <div className="flex items-center gap-3">
         {/* <Skills /> */}
-        <button className="py-2 px-4 border-2 border-black rounded-md font-medium ">Download CV</button>
+        <button className="py-2 px-4 border bg-white font-[450] border-gray-300 rounded-full flex items-center gap-1">Download CV <RxDownload className="font-bold text-[0.8rem]" /> </button>
+        <Social />
       </div>
     </section>
   )
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,10 +2,10 @@ import { socialPages } from '../data';
 
 function Social() {
   return (
-    <div>
-      <ul className='list-none flex gap-3 text-3xl mb-5'>
+    <div className='flex items-center'>
+      <ul className='list-none flex gap-2 text-xl'>
         {socialPages.map((item, index) => (
-          <li key={`social-${index}`}>{<item.icon />}</li>
+          <li className='border bg-white border-gray-300 rounded-full p-2' key={`social-${index}`}>{<item.icon />}</li>
         ))}
       </ul>
     </div>
