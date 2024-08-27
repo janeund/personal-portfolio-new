219edeb1878fd1e5904682a7ea599d018ee0cd5d
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-08-27 13:28:46 +0200

    fix: fix typo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer>
-      <p className="p-3 text-right text-sm text-slate-500">Build with React, Tailwind and Vite</p>
+      <p className="p-3 text-right text-sm text-slate-500">Built with React, Tailwind and Vite.</p>
     </footer>
   )
 }
