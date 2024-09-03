f87c0c7d65bdb75c2b2ab7f1a14863421a28dffa
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-09-03 11:22:23 +0200

    fix: change social links style to be able click on all area around icon iside button

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -5,8 +5,8 @@ function Social() {
     <div className='flex items-center'>
       <ul className='list-none flex gap-2 text-xl'>
         {socialPages.map((item, index) => (
-          <li className='cursor-pointer border hover:bg-stone-950 hover:text-white ease-in-out duration-500 bg-white border-gray-300 rounded-full p-3' key={`social-${index}`}>
-            <a href={item.link} target='_blank'>{<item.icon />}</a>   
+          <li className='flex items-center cursor-pointer border hover:bg-stone-950 hover:text-white ease-in-out duration-500 bg-white border-gray-300 rounded-full' key={`social-${index}`}>
+            <a className='inline-block w-full h-full p-3' href={item.link} target='_blank'>{<item.icon />}</a>   
           </li>
         ))}
       </ul>
